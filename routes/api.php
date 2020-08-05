22d9a08f0740d445e6d6bf09d72413db8b7a233a
Izakharchenko <ipzakharchenko@gmail.com>
2020-08-05 15:42:15 +0300

    Add method fallback for non-exist routes

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::fallback(function () {
+    return response()->json(['message' => 'Resource not found.'], 404);
+});
 
 Route::post('login', 'AuthController@login');
 
