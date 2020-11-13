5dfce67891b48dfa9c37a92f235ee341afecc231
Izakharchenko <ipzakharchenko@gmail.com>
2020-11-13 18:30:57 +0200

    Add migrations areas, regions, cities, strrets

---
+++ database/migrations/2020_11_13_154435_create_streets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStreetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('streets', function (Blueprint $table) {
+            $table->id();
+            $table->string('street_name');
+            $table->string('building_number', 45);
+            $table->foreignId('city_id')->constrained('cities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('streets');
+    }
+}
