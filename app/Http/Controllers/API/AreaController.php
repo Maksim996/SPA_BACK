c84f4a0bd53d30ecb4078224e995172097f88fb4
Izakharchenko <ipzakharchenko@gmail.com>
2020-11-23 21:36:27 +0200

    Add store method & validation request

--- app/Http/Controllers/API/AreaController.php
+++ app/Http/Controllers/API/AreaController.php
@@ -4,26 +4,43 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Area; // need move to dir Models;
+use App\Http\Requests\Address\StoreArea;
+use App\Area; // !need move to dir Models;
 
 class AreaController extends Controller
 {
+    /**
+     * Display a listing area.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return Area::all();
+        return Area::select('id', 'area_name')->orderBy('area_name')->get();
     }
+
     /**
      * Show address
      *
      * @param integer $id
-     * @return json
+     * @return Response
      */
     public function show($id)
     {
         return Area::findOrFail($id);
     }
 
-    public function create(Request $request) {
-        //
+    /**
+     * Store a newly area.
+     *
+     * @param StoreArea $request
+     * @return Response
+     */
+    public function store(StoreArea $request) {
+        $validated = $request->validated();
+
+        $area = new Area;
+        $area->area_name = $validated['area_name'];
+        $area->save();
     }
 }
