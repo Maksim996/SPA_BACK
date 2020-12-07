cbc0565f4118dfa452a3a4fbf7eb49c3cf9ec289
Izakharchenko <ipzakharchenko@gmail.com>
2020-12-07 21:01:48 +0200

    Fix indexes and move region_id after id

--- database/migrations/2020_11_13_154359_create_cities_table.php
+++ database/migrations/2020_11_13_154359_create_cities_table.php
@@ -16,7 +16,8 @@ class CreateCitiesTable extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('city_name', 150);
-            $table->foreignId('region_id')->constrained('regions');
+            $table->unique(['city_name', 'region_id']);
+            $table->foreignId('region_id')->constrained('regions')->after('id');
             $table->timestamps();
         });
     }
