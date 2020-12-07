a61c44cf73d5d8378e5b3785bc6e4f84d4bdd2e4
Izakharchenko <ipzakharchenko@gmail.com>
2020-12-07 20:56:45 +0200

    Add validation for store Cities

---
+++ app/Http/Requests/Address/StoreCity.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Address;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreCity extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'region_id' => 'required|integer|exists:App\Region,id',
+            'city_name' => [
+                'required',
+                'string',
+                'max:150',
+                Rule::unique('App\City')->where('region_id', $this->region_id)
+            ]
+        ];
+    }
+}
