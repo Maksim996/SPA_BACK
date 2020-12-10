ce928ca760f53ff93d141bc9ed69fba9e0d7fa76
Izakharchenko <ipzakharchenko@gmail.com>
2020-12-10 18:41:46 +0200

    Delete streets add addresses table

--- database/migrations/2020_12_09_195657_create_addresses_table.php
+++ database/migrations/2020_12_09_195657_create_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStreetsTable extends Migration
+class CreateAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateStreetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('streets', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('street_name');
-            $table->string('building_number', 45);
-            $table->foreignId('city_id')->constrained('cities');
+            $table->string('street');
+            $table->string('building', 45);
+            $table->foreignId('city_id')->constrained('cities')->after('id');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateStreetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('streets');
+        Schema::dropIfExists('addresses');
     }
 }
